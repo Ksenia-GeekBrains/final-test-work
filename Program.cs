01dcebb1b7ea7ad16a2c580695484fcf2915fe18
unknown <darkshariza@gmail.com>
2022-07-30 17:25:51 +1100

    Финал2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,1 @@
-﻿string newpath = @"C:\Users\ksenya\Desktop\FinalTestWork\input.txt";
-string [] ReadText = new string [] {newpath};
-
-    /* Split(' ', ',', ':', ';' } , StringSplitOptions.RemoveEmptyEntries; */
-
-Console.WriteLine(ReadText+"");
+﻿
