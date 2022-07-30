08a9bf94decb98a76f249246faf889c158fe3459
unknown <darkshariza@gmail.com>
2022-07-30 16:48:14 +1100

    Задание выполнено в черновике

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,9 +1,37 @@
-﻿string newpath = @"C:\Users\ksenya\Desktop\FinalTestWork\Test\input.txt";
-
-string [] Text = File.ReadAllText(newpath)
-.Split(new char[]{' ', ',', ':', ';','.','!'}
-, StringSplitOptions.RemoveEmptyEntries); 
-foreach (string item in Text)
+﻿string [] CreateArray(string path)
 {
-    Console.WriteLine(item);
+    string[] Text = File.ReadAllText(path)
+    .Split(new char[] { ' ', ',', ':', ';', '.', '!' }
+    , StringSplitOptions.RemoveEmptyEntries);
+    return Text;
 }
+
+string MainTask(string []array)
+{
+    int count = 0;
+    int index = 0;
+    string[] TempText = new string[0];
+
+    while (count < array.Length)
+    {
+        if (array[count].Length <= 3)
+        {
+            Array.Resize(ref TempText, TempText.Length + 1);
+            TempText[index] = array[count];
+            index++;
+        }
+        count++;
+    }
+string FinalText = String.Join(",", TempText);
+return FinalText;
+}
+
+void WriteArrayInFile(string FinalText)
+{
+    File.WriteAllText(@"C:\Users\ksenya\Desktop\FinalTestWork\Test\output.txt", FinalText);
+}
+
+string path = @"C:\Users\ksenya\Desktop\FinalTestWork\Test\input.txt";
+
+
+WriteArrayInFile(MainTask(CreateArray(path)));
