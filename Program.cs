e94aac82b2cf6c17a8809ce5a3cb38f82e36ba78
unknown <darkshariza@gmail.com>
2022-07-30 15:34:25 +1100

    Добавленo считывание  массива строк из файла  в ветке Черновик

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        string[] verse = new string {"Белеет парус одинокой В тумане моря голубом!.. Что ищет он в стране далекой? Что кинул он в краю родном?.. Играют волны - ветер свищет, И мачта гнется и скрыпит. Увы! он счастия не ищет И не от счастия бежит! Под ним струя светлей лазури, Над ним луч солнца золотой. А он, мятежный, просит бури, Как будто в бурях есть покой!"};}
-}
+﻿string newpath = @"C:\Users\ksenya\Desktop\FinalTestWork\input.txt";
+string [] ReadText = new string [] {newpath};
+
+    /* Split(' ', ',', ':', ';' } , StringSplitOptions.RemoveEmptyEntries; */
+
+Console.WriteLine(ReadText);
---
+++ Test/Program.cs
@@ -0,0 +1,9 @@
+﻿string newpath = @"C:\Users\ksenya\Desktop\FinalTestWork\Test\input.txt";
+
+string [] Text = File.ReadAllText(newpath)
+.Split(new char[]{' ', ',', ':', ';','.','!'}
+, StringSplitOptions.RemoveEmptyEntries); 
+foreach (string item in Text)
+{
+    Console.WriteLine(item);
+}
